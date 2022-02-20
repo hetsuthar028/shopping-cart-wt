1e153cd45a9035360ad8f4496812232bcfcfdee2
hetsuthar028 <hetmewada028@gmail.com>
2022-02-20 10:43:59 +0530

    Auth middlewares configured with all routers
    - Cart router
    - Order router
    - Product router

--- server/src/routes/cart-router.js
+++ server/src/routes/cart-router.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cartRouter = express.Router();
 const cartController = require("../cart/controller/cart.controller");
+const authMiddleware = require('../middlewares/index');
 
-cartRouter.post("/add/item", cartController.addItem);
-cartRouter.get("/get/items/:userId", cartController.getCartItemsByUserId);
-cartRouter.delete("/delete/item/:itemId", cartController.deleteItemById);
+cartRouter.post("/add/item", authMiddleware.isLoggedIn, cartController.addItem);
+cartRouter.get("/get/items/:userId", authMiddleware.isLoggedIn, cartController.getCartItemsByUserId);
+cartRouter.delete("/delete/item/:itemId", authMiddleware.isLoggedIn, cartController.deleteItemById);
 
 module.exports = cartRouter;
--- server/src/routes/order-router.js
+++ server/src/routes/order-router.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const orderController = require('../order/controller/order.controller');
-
+const authMiddleware = require('../middlewares/index');
 const orderRouter = express.Router();
 
-orderRouter.post('/purchase', orderController.purchase);
-orderRouter.get('/get/id/:userId', orderController.getMyOrders);
+orderRouter.post('/purchase', authMiddleware.isLoggedIn, orderController.purchase);
+orderRouter.get('/get/id/:userId', authMiddleware.isLoggedIn, orderController.getMyOrders);
 
 module.exports = orderRouter;
